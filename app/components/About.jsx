e6237c368e8c8d216f14a5606e010728598852f5
Raphael Guimaraes <raphael.guimaraes@stormsec.com.br>
2016-08-03 17:13:54 -0300

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,6 +13,9 @@ var About = (props) => {
 					<li>
 						<a href="https://openweathermap.org">Open Weather Map</a> - I used Open Weather Map API to search for weather data by city name.
 					</li>
+					<li>
+						<a href="http://foundation.zurb.com/">Foudation</a> - This was the front-end framework used.
+					</li>
 				</ul>
 			</p>
 		</div>
